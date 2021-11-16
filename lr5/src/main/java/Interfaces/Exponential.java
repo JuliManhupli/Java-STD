20d3abc9b819273874939c174c3cfe7080fda348
JuliZliuka <juliamangup@gmail.com>
2021-11-16 16:49:42 +0200

    Create Exponential.java

---
+++ lr5/src/main/java/Interfaces/Exponential.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public interface Exponential {
+    String get_standard_form();
+    void set_standard_form(String standard_form);
+    String get_exponential_form();
+    void set_exponential_form(String exponential_form);
+    String toString();
+    /**
+     * Function that defines a number in exponential form
+     * @return returns a number in exponential form
+     */
+    String Calculation_exponential_form();
+}
