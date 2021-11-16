0f6a6deda5fcd157927627d2a082e904dd385e70
JuliZliuka <juliamangup@gmail.com>
2021-11-16 16:49:37 +0200

    Create Complex.java

---
+++ lr5/src/main/java/Interfaces/Complex.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface Complex {
+    void set_x(double x);
+    double get_x();
+    void set_y(double y);
+    double get_y();
+    void set_z(String z);
+    String get_z();
+    String toString();
+}
